3fe3dead6db4d5b7095ff540ec74e5d57448d60a
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-03-23 21:45:07 +0000

    updates

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-self.addEventListener('install', event => {
-  // Perform install steps
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('sw-cache').then(function(cache) {
+      return cache.add('index.html');
+    })
+  );
 });
-
-self.addEventListener('fetch', event => {
-  // Handle fetch events
+ 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
